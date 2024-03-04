2f17858a10ed793c3ea17cb13138b3702af9d8a3
Axel <axel.deb.len@gmail.com>
2024-03-04 11:51:35 +1100

    add dummy component for the meantime

--- src/components/ui/Accordion/Accordion.stories.tsx
+++ src/components/ui/Accordion/Accordion.stories.tsx
@@ -1,18 +1,20 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
 // import { Accordion } from './index';
+//
+const DummyComponent = () => <div> Hello World </div>
 
-// const meta = {
-//   title: 'Example/Accordion',
-//   component: Accordion,
-//   parameters: {
-//     layout: 'centered',
-//   },
-// } satisfies Meta<typeof Accordion>;
+const meta = {
+  title: 'Example/Accordion',
+  component: DummyComponent,
+  parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta<typeof DummyComponent>
 
-// export default meta
-// type Story = StoryObj<typeof meta>
+export default meta
+type Story = StoryObj<typeof meta>
 
-// export const Base: Story = {
-// render: (args) => (<Accordion { ...args }> Large </Accordion>)
-// }
+export const Base: Story = {
+  render: (args) => <DummyComponent {...args}> </DummyComponent>,
+}
