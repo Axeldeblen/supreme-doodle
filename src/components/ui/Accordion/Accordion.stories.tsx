a4ac6c86179563654c83dccf5f24a4c00e489112
Axel <axel.deb.len@gmail.com>
2024-03-04 11:48:06 +1100

    better setup - less noise

---
+++ src/components/ui/Accordion/Accordion.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+// import { Accordion } from './index';
+
+// const meta = {
+//   title: 'Example/Accordion',
+//   component: Accordion,
+//   parameters: {
+//     layout: 'centered',
+//   },
+// } satisfies Meta<typeof Accordion>;
+
+// export default meta
+// type Story = StoryObj<typeof meta>
+
+// export const Base: Story = {
+// render: (args) => (<Accordion { ...args }> Large </Accordion>)
+// }
