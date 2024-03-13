08648fde76d9d802a805f626decfff9a7b99089e
Axel <axel.deb.len@gmail.com>
2024-03-14 10:51:07 +1100

    add some dummy content

--- src/components/ui/Accordion/Accordion.stories.tsx
+++ src/components/ui/Accordion/Accordion.stories.tsx
@@ -4,6 +4,22 @@ import type { Meta, StoryObj } from '@storybook/react'
 //
 const DummyComponent = () => <div> Hello World </div>
 
+const dummyContent = [
+  {
+    title: 'First',
+    content: 'First content',
+  },
+  {
+    title: 'Second',
+    content:
+      'Second content Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi accusamus consequatur facere obcaecati corrupti facilis perferendis officiis dolores vitae fugit! Incidunt maxime vitae deserunt corporis cupiditate sunt repudiandae modi explicabo.',
+  },
+  {
+    title: 'Third',
+    content: 'Third content',
+  },
+]
+
 const meta = {
   title: 'Example/Accordion',
   component: DummyComponent,
